e506c5c4ce3e7a7e384a41e7fc66a6c2de7aff26
Sayantan Mukherjee <135653458+Say2hub@users.noreply.github.com>
2025-03-20 18:05:46 +0530

    Add files via upload

---
+++ Reflections_2.java
@@ -0,0 +1,33 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.AnnotatedElement;
+
+// Define the annotation
+@Retention(RetentionPolicy.RUNTIME)
+@interface Info {
+    String author();
+    double version();
+}
+
+// Apply the annotation to Library class
+@Info(author = "John Doe", version = 1.2)
+class Library {
+}
+
+public class Reflections_2 {
+    public static void main(String[] args) {
+        Class<Library> libraryClass = Library.class;
+
+        // Check if annotation exists
+        if (libraryClass.isAnnotationPresent(Info.class)) {
+            System.out.println("Library class is annotated with @Info");
+
+            // Retrieve annotation values
+            Info info = libraryClass.getAnnotation(Info.class);
+            System.out.println("Author: " + info.author());
+            System.out.println("Version: " + info.version());
+        } else {
+            System.out.println("Library class is not annotated with @Info");
+        }
+    }
+}
